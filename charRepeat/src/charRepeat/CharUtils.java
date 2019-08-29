92cb3f5fff3ed87cba005390194921031089a964
Aaron Cox <acox@192.168.1.24>
2019-08-28 20:22:46 -0400

    Optimize

--- charRepeat/src/charRepeat/CharUtils.java
+++ charRepeat/src/charRepeat/CharUtils.java
@@ -9,22 +9,29 @@ public class CharUtils {
 	
 	public int printNumRepeatCharNoDups(String inputString) {
 		
-		if(inputString == null) {
+		if(inputString == null  || inputString.length()<2) {
 			//Log statement
-			System.out.println("Number of unique duplicate adjacent characters for string null: 0");
+			//Log statement
+			System.out.println("Number of unique duplicate adjacent "
+								+ "characters for string \"" 
+								+ inputString + "\": 0");
 			return 0;
 		}
 		
 		Set<String> countMap = new HashSet<String>();
-		int position =1;
 		char[] inputStringChars = inputString.toCharArray();
 		
-		for(char c: inputStringChars) {
-			if(position<inputStringChars.length && c == inputStringChars[position]) {
-				countMap.add(c+"");
+		char current = inputStringChars[0];
+		
+		for(int i =0; i<inputStringChars.length-1;i++) {
+			
+			char next = inputStringChars[i+1];
+			
+			if(current==next) {
+				countMap.add(current+"");
 			}
-			position++;
-
+			
+			current = next;
 		}
 		
 		//Log statement
