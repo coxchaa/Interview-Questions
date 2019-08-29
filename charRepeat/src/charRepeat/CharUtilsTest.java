7b9491cdb169077ab70244fb4d73319cf6d76a9e
(Charles) Aaron Cox <charles.cox@vonage.com>
2019-08-28 20:25:08 -0400

    Optimize (#5)

--- charRepeat/src/charRepeat/CharUtilsTest.java
+++ charRepeat/src/charRepeat/CharUtilsTest.java
@@ -2,10 +2,31 @@ package charRepeat;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class CharUtilsTest {
 
+	static String longString = "abcd";
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		StringBuilder sb = new StringBuilder();
+		sb.append(longString);
+		for(int i=0;i<20;i++) {
+			sb.append(sb.toString());
+		}
+		longString = sb.toString();
+	}
+	
+	@Test
+	void charRepeatTest_LongStringTest() {
+		CharUtils charUtils = new CharUtils();
+		String test = longString;
+		int val = charUtils.printNumRepeatCharNoDups(test);
+		assertEquals(val, 0);
+	}
+	
 	@Test
 	void charRepeatTest_ZeroMatch() {
 		CharUtils charUtils = new CharUtils();
@@ -50,6 +71,14 @@ class CharUtilsTest {
 	void charRepeatTest_NullInput() {
 		CharUtils charUtils = new CharUtils();
 		String test = null;
+		int val = charUtils.printNumRepeatCharNoDups(test);
+		assertEquals(val, 0);
+	}
+	
+	@Test
+	void charRepeatTest_OneChar() {
+		CharUtils charUtils = new CharUtils();
+		String test = "a";
 		int val = charUtils.printNumRepeatCharNoDups(test);
 		assertEquals(val, 0);
 	}
